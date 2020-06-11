8b0bf4bc03f80fcbc0251caff1612db3beec1bd3
Mario Jason Braganza <jason@janusworx.com>
2020-06-11 16:56:17 +0530

    Baby steps. Learnt about templates and conditionals
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return f"Hello World!"
+    user = {"username": "mjb"}
+    return render_template("index.html", title="Home", user=user)
