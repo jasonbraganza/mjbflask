d6f1f2d324dc04f8fee1b642e5f4d407486ce6b2
Mario Jason Braganza <jason@janusworx.com>
2020-06-12 13:24:21 +0530

    Loops and templates
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,8 @@ from app import app
 @app.route("/index")
 def index():
     user = {"username": "mjb"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {"author": {"username": "Mario"}, "body": "Beautiful, rainy, day at home."},
+        {"author": {"username": "Jason"}, "body": "Goodness, what a downpour"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
