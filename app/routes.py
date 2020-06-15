0884d1c9561b9dc9fd99d1f8e69faa1871a02da1
Mario Jason Braganza <jason@janusworx.com>
2020-06-15 10:21:33 +0530

    wip_commit
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -11,3 +12,14 @@ def index():
         {"author": {"username": "Jason"}, "body": "Goodness, what a downpour"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Hello {form.username.data}, you have successfully logged in. Remember me set to {form.remember_me.data}"
+        )
+        return redirect("/index")
+    return render_template("login.html", title="Login Page", form=form)
