d87b3d458b10d4a8288cc26b2d82e148e68f3995
Mario Jason Braganza <jason@janusworx.com>
2020-06-16 10:12:32 +0530

    Still debugging
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(
-            f"Hello {form.username.data}, you have successfully logged in. Remember me set to {form.remember_me.data}"
+            "Hello {}, you have successfully logged in. Remember me set to {}".format(
+                form.username.data, form.remember_me.data
+            )
         )
         return redirect("/index")
-    return render_template("login.html", title="Login Page", form=form)
+    return render_template("login.html", title="Sign In", form=form)
